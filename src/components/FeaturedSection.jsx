f0405b2ac7159c91da125ae04766363596df447e
Divyanshu <divyanshutiwari173@gmail.com>
2025-06-29 02:28:23 +0530

    animated FeaturedSection

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -3,30 +3,37 @@ import BlurCircle from './BlurCircle'
 import { useNavigate } from 'react-router-dom'
 import { dummyShowsData } from '../assets/assets'
 import MovieCard from './MovieCard'
+import { motion } from "framer-motion";
 
 const FeaturedSection = () => {
     const navigate = useNavigate();
     return (
-        <div className='flex flex-col px-[5vw] sm:px-[6vw] md:px-[6vw] lg:px-[10vw] overflow-hidden mt-[3vh] '>
-            <div className='flex items-center justify-between'>
+        <motion.div 
+         initial={{opacity:0,y:40}}
+         whileInView={{opacity:1,y:0}}
+         transition={{duration:0.8}}
+
+
+         className='flex flex-col px-[5vw] sm:px-[6vw] md:px-[6vw] lg:px-[10vw] overflow-hidden mt-[3vh] '>
+            <motion.div className='flex items-center justify-between'>
                 <h5 className='text-[#D1D5DC] font-bold text-[1.3rem] md:text-[1.8rem]'>Now Showing</h5>
-                <div className='flex items-center text-sm gap-2'>
+                <motion.div className='flex items-center text-sm gap-2'>
                     <button className='cursor-pointer' onClick={() => navigate('/movies')}>View All </button>
                     <FaArrowRight className='' />
-                </div>
+                </motion.div>
                 <BlurCircle top='20' right={20} />
-            </div>
-            <div className='flex gap-5 flex-wrap justify-center md:justify-between mt-6'>
+            </motion.div>
+            <motion.div className='flex gap-5 flex-wrap justify-center md:justify-between mt-6'>
                 {dummyShowsData.slice(0,6).map((show) => (
                     <MovieCard key={show.id} movie={show} />
                 ))}
-            </div>
+            </motion.div>
 
-            <div className='mt-10 flex justify-center'>
+            <motion.div className='mt-10 flex justify-center'>
                 <button onClick={()=>(navigate('/movies'))} className='w-[6rem] md:8rem bg-primary rounded-[6px] p-1.5 cursor-pointer'>Show More</button>
-            </div>
+            </motion.div>
 
-        </div>
+        </motion.div>
     )
 }
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,17 @@
 import { FaStar } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import getHours from '../lib/getHours';
+import { motion } from 'framer-motion';
+
 
 const MovieCard = ({movie}) => {
     const navigate = useNavigate();
     return (
-        <div className='relative p-3 w-[268px] h-[356px] bg-[#12161C] rounded-[16px] flex flex-col gap-[1rem]'>
+        <motion.div
+        initial={{opacity:0,y:30}}
+        whileInView={{opacity:1,y:0}}
+        transition={{duration:0.6}}
+        className='relative p-3 w-[268px] h-[356px] bg-[#12161C] rounded-[16px] flex flex-col gap-[1rem]'>
             <img onClick={()=>(navigate(`/movies/:${movie.id}`))} className='bg-cover bg-center rounded-[6px] h-[161px]' src={movie.backdrop_path} alt="movie" />
             <p className='h-[38px] font-[Heebo] font-bold leading-[117%] align-middle'>{movie.title}</p>
             <div><p className='font-[Heebo] text-[14px] font-semibold text-[#797B7D] align-middle'>{`${new Date(movie.release_date).getUTCFullYear()} - ${movie.genres.slice(0,2).map(genre=>genre.name).join(" | ")} - ${getHours(movie.runtime)}`}</p></div>
@@ -16,7 +22,7 @@ const MovieCard = ({movie}) => {
                     <p className='text-[#797B7D] text-[16px]'>{movie.vote_average.toFixed(1)}</p>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
