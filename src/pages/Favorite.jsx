fd23404201bbdfcf17f18856a8da08066f9ce01b
Divyanshu <divyanshutiwari173@gmail.com>
2025-06-30 13:43:41 +0530

    implemented fully responsive Favorite page

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard'
+import BlurCircle from '../components/BlurCircle'
 
 const Favorite = () => {
-  return (
-    <div>Favorite</div>
-  )
+  return dummyShowsData.length > 0 ? (
+    <div className='flex flex-col gap-[3vh] px-[4vw] my-[20vh]'>
+      <h1 className='font-bold text-[20px] sm:text-[16px]'>Fevorites</h1>
+      <BlurCircle top={0} right={0} />
+      <div className='flex gap-3 flex-wrap'>
+        {dummyShowsData.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <BlurCircle bottom={0} left={0} />
+    </div>) : <div className='flex gap-[3vh] px-[4vw] my-[20vh] justify-center'><h1 className='font-bold text-[20px] sm:text-[16px]'>No fevorite Movies to display</h1></div>
 }
 
 export default Favorite
