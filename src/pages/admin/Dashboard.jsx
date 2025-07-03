e8b20527ac4ddef5a6ad9080630b8a5a3e4f9424
Divyanshu <divyanshutiwari173@gmail.com>
2025-07-04 00:48:50 +0530

    created admin ListShows page

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,19 +1,41 @@
 import { IoIosTrendingUp } from "react-icons/io";
 import MovieCard from '../../components/MovieCard'
 import { dummyDashboardData } from "../../assets/assets";
+import { MdOutlineAttachMoney } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { MdLocalMovies } from "react-icons/md";
+import { useEffect, useState } from "react";
+import Spinner from "../../components/Spinner";
+
 
 const Dashboard = () => {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0
+  });
+
+  const fetchData = async () => {
+    setDashboardData(dummyDashboardData);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+  return !isLoading ? (
     <div className='md:px-[6vw] md:py-[4vh] px-3 py-3 flex flex-col gap-6'>
       <div className=''>
         <h2 className='max-w-[206px] md:text-[26px] text-left flex gap-1'>Admin <span className='text-primary'>Dashboard</span></h2>
       </div>
-      <div className="flex flex-wrap gap-5">
+      <div className="flex flex-wrap gap-5 justify-center">
         <div>
           <div className='flex items-center justify-between px-6 py-2 bg-primary/30 w-[196px] h-[77px] rounded-[6px] border border-primary'>
             <div className='flex flex-col gap-2'>
               <p className='text-[#FFFFFF] text-[12px]'>Total Bookings</p>
-              <p>{dummyDashboardData.totalBookings}</p>
+              <p>{dashboardData.totalBookings}</p>
             </div>
             <IoIosTrendingUp className="text-[30px]" />
           </div>
@@ -22,42 +44,42 @@ const Dashboard = () => {
           <div className='flex items-center justify-between px-6 py-2 bg-primary/30 w-[196px] h-[77px] rounded-[6px] border border-primary'>
             <div className='flex flex-col gap-2'>
               <p className='text-[#FFFFFF] text-[12px]'>Total Revenue</p>
-              <p>{dummyDashboardData.totalRevenue}</p>
+              <p>{dashboardData.totalRevenue}</p>
             </div>
-            <IoIosTrendingUp className="text-[30px]" />
+            <MdOutlineAttachMoney className="text-[30px]" />
           </div>
         </div>
         <div>
           <div className='flex items-center justify-between px-6 py-2 bg-primary/30 w-[196px] h-[77px] rounded-[6px] border border-primary'>
             <div className='flex flex-col gap-2'>
               <p className='text-[#FFFFFF] text-[12px]'>Active Movies</p>
-              <p>{dummyDashboardData.activeShows.length}</p>
+              <p>{dashboardData.activeShows.length}</p>
             </div>
-            <IoIosTrendingUp className="text-[30px]" />
+            <MdLocalMovies className="text-[30px]" />
           </div>
         </div>
         <div>
           <div className='flex items-center justify-between px-6 py-2 bg-primary/30 w-[196px] h-[77px] rounded-[6px] border border-primary'>
             <div className='flex flex-col gap-2'>
               <p className='text-[#FFFFFF] text-[12px]'>Total Users</p>
-              <p>{dummyDashboardData.totalUser}</p>
+              <p>{dashboardData.totalUser}</p>
             </div>
-            <IoIosTrendingUp className="text-[30px]" />
+            <FaUsers className="text-[30px]" />
           </div>
         </div>
       </div>
-      <div>
+      <div className="flex flex-col gap-10">
         <div className="mt-10">
           <h2>Active Movies</h2>
         </div>
-        <div className="flex flex-wrap gap-5">
-          {dummyDashboardData.activeShows.slice(0,3).map((item) => (
-               <MovieCard key={item._id} movie={item.movie} />
+        <div className="flex flex-wrap gap-5 justify-center">
+          {dashboardData.activeShows.slice(0, 3).map((item) => (
+            <MovieCard key={item._id} movie={item.movie} />
           ))}
         </div>
       </div>
     </div>
-  )
+  ) : <Spinner/>
 }
 
-export default Dashboard
+export default Dashboard;
--- src/pages/admin/ListShows.jsx
+++ src/pages/admin/ListShows.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { dummyShowsData } from "../../assets/assets";
+import Spinner from "../../components/Spinner";
+import dateFormat from "../../lib/dateFormat";
+
 
 const ListShows = () => {
-  return (
-    <div>ListShows</div>
-  )
+  const [showsData, setShowsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getData = async () => {
+    setShowsData([{
+      movie: dummyShowsData[0],
+      showDateTime: "2025-07-24T01:00:00.000Z",
+      showPrice: 59,
+      occupiedSeats: {
+        A1: 'User1',
+        A2: 'User2',
+        A3: 'User3'
+      }
+    }]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return !loading ? (
+    <div className='md:px-[6vw] md:py-[4vh] px-3 py-3 flex flex-col gap-6'>
+      <div>
+        <h2 className='max-w-[206px] md:text-[26px] text-left flex gap-1'>List<span className='text-primary'>Shows</span></h2>
+      </div>
+      <div>
+        <div >
+          <table className='w-full border-collapse bg-primary/25 border border-primary'>
+            <thead className="bg-primary/40 text-white">
+              <tr>
+                <th className="px-4 py-2 text-left">Movie Name</th>
+                <th className="px-4 py-2 text-left">Date</th>
+                <th className="px-4 py-2 text-left">Total Booking</th>
+                <th className="px-4 py-2 text-left">Earning</th>
+              </tr>
+            </thead>
+            <tbody>
+              {showsData.map((item, index) => (
+                <tr className="border-b border-primary/40">
+                  <td className="px-4 py-2">{item.movie.title}</td>
+                  <td className="px-4 py-2">{dateFormat(item.showDateTime)}</td>
+                  <td className="px-4 py-2">{Object.keys(item.occupiedSeats).length}</td>
+                  <td className="px-4 py-2">{`₹${Object.keys(item.occupiedSeats).length * item.showPrice}`}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  ) : <Spinner />
 }
 
 export default ListShows
